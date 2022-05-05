2aaf67fb6e771de212b4aeafe1c596965024d08e
Shwetakotak <shwetakotak1@gmail.com>
2022-05-05 14:09:11 -0400

    updated apiKey and img

--- src/app.js
+++ src/app.js
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "6d07e05990c7b63e3c50ddc2f8b42089";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
